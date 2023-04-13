0d640e2ef7faebe7a7e969af63917d6e093433e1
Jose Paolo Sese <jpaolo.sese@gmail.com>
2023-04-13 10:43:07 -0700

    Add tests WIP

---
+++ src/Components/__tests__/Reimbursements.test.tsx
@@ -0,0 +1,16 @@
+import Reimbursements from "../Reimbursements";
+import axios from "axios";
+import { render } from "@testing-library/react";
+
+jest.mock('axios')
+
+
+describe("View reimbursements", () => {
+    test("renders reimbursements as employee", () => {
+
+    })
+
+    test("renders reimbursements as manager", () => {
+
+    })
+})
